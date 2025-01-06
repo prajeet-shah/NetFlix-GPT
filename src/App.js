f624e60fed4f67178944d189edf94e05eba602c2
Prajeet Shah <prajeetshah93@gmail.com>
2025-01-06 16:51:12 +0545

    create-react-app & configure tailwindcss

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p className="text-center text-3xl">
+        it's prajeet shah, building the Netflix GPT
+      </p>
+    </div>
+  );
+}
+
+export default App;
